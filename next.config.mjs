30c667709d86effd9424714964f82de0aa1adbe9
MohanMaharjan <mohan@achsnapal.com>
2025-06-26 08:07:42 +0545

    Fixed Prisma build issues and import errors 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     images: {
-    domains: ['images.unsplash.com','islington.edu.np','iimscollege.edu.np'],
-  },
+ 
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.externals.push("@prisma/client"); // Avoids Vercel bundling issues
+      config.externals.push('@prisma/client'); // Avoids Vercel bundling issues
     }
     return config;
   },
